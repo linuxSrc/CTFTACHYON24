77dfe58372908f294cb9ce075638ece702712057
suraj21 <surajyadav2121115@gmail.com>
2024-11-15 01:17:46 +0530

    responsive changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,29 @@
 // src/components/HomePage.js
-import React from 'react';
+import React, { useState } from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="home-container">
       <div className="overlay">
         <nav className="navbar">
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/signup">Sign Up</a></li>
-          </ul>
+          <div className="navbar-content">
+            <div className="logo">BOTNET</div>
+            <ul className={`navbar-links ${menuOpen ? 'navbar-links-visible' : ''}`}>
+              <li><a href="/">Home</a></li>
+              <li><a href="/login">Login</a></li>
+              <li><a href="/signup">Sign Up</a></li>
+            </ul>
+            <div className="hamburger" onClick={toggleMenu}>
+              &#9776; {/* Hamburger icon */}
+            </div>
+          </div>
         </nav>
         <div className="home-content">
           <h1>Welcome to CHAKARAVYUH-1.0 CTF</h1>
